b73b5fa922446532b7ac630cb25f6b086888ef5a
s3yox <seyox055@gmail.com>
2024-12-04 01:29:25 +0300

    Infosection.vue olusturuldu

---
+++ nuxt-app/components/Infosection.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="info-section">
+      <div class="row">
+        <div class="col">
+          <img src="https://imagizer.imageshack.com/img922/6973/KOG6Kz.png" />
+          750 TL ÜZERİ<br />KARGO BEDAVA
+        </div>
+        <div class="col">
+          <img src="https://static.ticimax.cloud/cdn-cgi/image/width=23,quality=99,format=auto/2571/customcss/images/g22.png" />
+          %100 GÜVENLİ<br />ÖDEME SİSTEMİ
+        </div>
+        <div class="col">
+          <img src="https://static.ticimax.cloud/cdn-cgi/image/width=27,quality=99,format=auto/2571/customcss/images/g44.png" />
+          CANLI DESTEK <br />0850 888 78 00
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  // TypeScript kodu ekleyebilirsiniz
+  </script>
+  
+  <style scoped>
+  .info-section {
+    text-align: center;
+    margin-top: 40px;
+  }
+  
+  .row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .col {
+    flex: 1;
+    text-align: center;
+  }
+  
+  .col img {
+    width: 43px;
+  }
+  </style>
+  
